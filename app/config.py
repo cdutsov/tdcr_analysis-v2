a806e2a9da46ecc3457a20c58eb5448a17ed66ab
cdutsov <chavcho93@gmail.com>
2017-04-08 15:11:05 +0300

    initial commit

--- app/config.py
+++ app/config.py
@@ -5,7 +5,10 @@ UPLOAD_FOLDER = 'data/'
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tdcr_database.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tdcr_database.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tdcr_database.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'tdcr_db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
