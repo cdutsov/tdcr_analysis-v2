6ba8cc63e017dcf2bb496ddcc47fea530d0dff62
cdutsov <chavcho93@gmail.com>
2017-04-11 17:38:24 +0300

    add user add

--- create_user.py
+++ create_user.py
@@ -1,10 +1,9 @@
 #!flask/bin/python
+import sys
 from app import db
 from app.models import User
 
-
-def create_user(username, password):
-    user = User(username=username, password=password)
-    db.session.add(user)
-    db.session.commit()
-    print("user created:", user.username)
+user = User(username=sys.argv[1], password=sys.argv[2])
+db.session.add(user)
+db.session.commit()
+print("user created: ", user.username, " with password: ", sys.argv[2])
