1fac661f52f07d4f5f56d425d6246584b1e4d6e8
cdutsov <chavcho93@gmail.com>
2017-04-11 18:08:27 +0300

    add user add

--- create_user.py
+++ create_user.py
@@ -10,7 +10,7 @@ user = User(username=username, password=password)
 if not os.path.exists(os.path.join(UPLOAD_FOLDER, username)):
     os.mkdir(os.path.join(UPLOAD_FOLDER, username))
 
-if not os.path.exists(UPLOAD_FOLDER, username, 'Exports'):
+if not os.path.exists(os.path.join(UPLOAD_FOLDER, username, 'Exports')):
     os.mkdir(os.path.join(UPLOAD_FOLDER, username, 'Exports'))
 
 db.session.add(user)
