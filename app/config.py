9aa6341db541edfabc61b0cf70e3b9ce47e15ad1
Chavdar Dutsov <cdutsov@github.com>
2017-04-12 11:20:09 +0300

    commit only important files

---
+++ app/config.py
@@ -0,0 +1,14 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'n49RtlwUYxYmhqW8OhM6c67nIAlI63Su'
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tdcr_database.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tdcr_database.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'tdcr_db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+UPLOAD_FOLDER = os.path.join(basedir, 'data/')
